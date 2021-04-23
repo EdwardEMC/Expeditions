4ae5f0dfc9c55f99eec1fc614876db81a18dbd1a
Edward Coad <coaem001@mymail.unisa.edu.au>
2021-04-23 12:56:49 +0930

    WebXR added, guide and peer seperated

---
+++ public/assets/webxr/webxr.js
@@ -0,0 +1,216 @@
+import {WebXRButton} from '/assets/webxr/util/webxr-button.js';
+import {Scene} from '/assets/webxr/render/scenes/scene.js';
+import {Renderer, createWebGLContext} from '/assets/webxr/render/core/renderer.js';
+import {InlineViewerHelper} from '/assets/webxr/util/inline-viewer-helper.js';
+
+export class WebXR {
+  constructor(options) {
+    // Application options and defaults
+    if (!options) { options = {}; }
+
+    this.options = {
+      inline: 'inline' in options ? options.inline : true,
+      immersiveMode: options.immersiveMode || 'immersive-vr',
+      referenceSpace: options.referenceSpace || 'local',
+      defaultInputHandling: 'defaultInputHandling' in options ? options.defaultInputHandling : true
+    };
+
+    this.account = options.account;
+    this.gl = null;
+    this.renderer = null;
+    this.scene = new Scene();
+    this.mobile_vr = true;
+    this.session = null;
+    this.XRFrame = options.XRFrame || null;
+    this.pointer = options.pointer || null;
+
+    this.xrButton = new WebXRButton({
+      onRequestSession: () => { return this.onRequestSession(); },
+      onEndSession: (session) => { this.onEndSession(session); }
+    });
+
+    this.immersiveRefSpace = null;
+    this.inlineViewerHelper = null;
+
+    this.frameCallback = (time, frame) => {
+      let session = frame.session;
+      let refSpace = this.getSessionReferenceSpace(session);
+      session.requestAnimationFrame(this.frameCallback);
+      this.scene.startFrame();
+
+      // determine if running the menu scene or the tour scene
+      // keep for now incase extra functionality is needed later
+      window.location.pathname === "/Tour" ? this.XRFrame(time, frame, refSpace) : this.onXRFrame(time, frame, refSpace)
+
+      this.scene.endFrame();
+    };
+  }
+
+  getSessionReferenceSpace(session) {
+    return session.isImmersive ? this.immersiveRefSpace : this.inlineViewerHelper.referenceSpace;
+  }
+
+  run() {
+    this.onInitXR();
+  }
+
+  onInitXR() {
+    if (navigator.xr) {
+      navigator.xr.isSessionSupported('immersive-vr').then((supported) => {
+        this.xrButton.enabled = supported;
+      });
+
+      // Request an inline session if needed.
+      if (this.options.inline) {
+        navigator.xr.requestSession('inline').then((session) => {
+          this.onSessionStarted(session);
+        });
+      }
+    }
+  }
+
+  onCreateGL() {
+    return createWebGLContext({
+      xrCompatible: true
+    });
+  }
+
+  onInitRenderer() {
+    
+    if (this.gl)
+      return;
+
+    this.gl = this.onCreateGL();
+
+    if(this.gl) {
+      let canvas = this.gl.canvas;
+      canvas.setAttribute("id", "main_canvas");
+      if (canvas instanceof HTMLCanvasElement) {
+        document.body.append(this.gl.canvas);
+        function onResize() {
+          canvas.width = canvas.clientWidth * window.devicePixelRatio;
+          canvas.height = canvas.clientHeight * window.devicePixelRatio;
+        }
+        window.addEventListener('resize', onResize);
+        onResize();
+      }
+
+      this.renderer = new Renderer(this.gl);
+      this.scene.setRenderer(this.renderer);
+    }
+  }
+
+  onRequestSession() {
+    // Called when the VR button gets clicked. Requests an immersive session.
+    return navigator.xr.requestSession(this.options.immersiveMode, {
+        requiredFeatures: [this.options.referenceSpace]
+    }).then((session) => {
+      this.xrButton.setSession(session);
+      session.isImmersive = true;
+      this.onSessionStarted(session);
+    });
+  }
+
+  onEndSession(session) {
+    session.end();
+  }
+
+  onSessionStarted(session) {
+    session.addEventListener('end', (event) => {
+      this.onSessionEnded(event.session);
+    });
+
+    session.addEventListener('select', this.pointer);
+
+    // enable selecton of icons
+    session.addEventListener('select', (event) => {
+      let refSpace = this.getSessionReferenceSpace(event.frame.session);
+      this.scene.handleSelect(event.inputSource, event.frame, refSpace);
+    });
+
+    this.onInitRenderer();
+
+    this.scene.inputRenderer.useProfileControllerMeshes(session);
+
+    session.updateRenderState({
+      baseLayer: new XRWebGLLayer(session, this.gl)
+    });
+
+    this.onRequestReferenceSpace(session).then((refSpace) => {
+      if (session.isImmersive) {
+        this.immersiveRefSpace = refSpace;
+      } else {
+        this.inlineViewerHelper = new InlineViewerHelper(this.gl.canvas, refSpace);
+        if (this.options.referenceSpace == 'local-floor' ||
+            this.options.referenceSpace == 'bounded-floor') {
+          this.inlineViewerHelper.setHeight(1.6);
+        }
+      }
+
+      session.requestAnimationFrame(this.frameCallback);
+    });
+  }
+
+  onRequestReferenceSpace(session) {
+    if (this.options.referenceSpace && session.isImmersive) {
+      return session.requestReferenceSpace(this.options.referenceSpace);
+    } else {
+      return session.requestReferenceSpace('viewer');
+    }
+  }
+
+  onSessionEnded(session) {
+    this.mobile_vr = false;
+    if (session == this.xrButton.session) {
+      this.xrButton.setSession(null);
+    }
+  }
+
+  // Override to customize frame handling
+  onXRFrame(time, frame, refSpace) {
+    let pose = frame.getViewerPose(refSpace);
+    if (this.options.defaultInputHandling) {
+      this.scene.updateInputSources(frame, refSpace);
+    }
+    this.scene.drawXRFrame(frame, pose);
+  }
+
+  // toggle fullscreen mode
+  toggleFullScreen() {
+    let vr_button = $('#vr_button');
+    if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+    (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+      vr_button.toggle();
+      if (document.documentElement.requestFullScreen) {  
+        document.documentElement.requestFullScreen();  
+      } else if (document.documentElement.mozRequestFullScreen) {  
+        document.documentElement.mozRequestFullScreen();  
+      } else if (document.documentElement.webkitRequestFullScreen) {  
+        document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+      }  
+    } else {  
+      vr_button.toggle();
+      if (document.cancelFullScreen) {  
+        document.cancelFullScreen();  
+      } else if (document.mozCancelFullScreen) {  
+        document.mozCancelFullScreen();  
+      } else if (document.webkitCancelFullScreen) {  
+        document.webkitCancelFullScreen();  
+      }  
+    }  
+  }
+
+  // add the fullscreen icon onto the screen
+  createIcon(path, id) {
+    let img = document.createElement("IMG");
+    img.src = "/assets/media/buttons/" + path; //fullscreen.png
+    img.alt = path;
+    img.setAttribute("id", id); //fullscreen_toggle
+    return img;
+  }
+
+  // hiding the player buttons
+  toggleOptions() {
+    console.log("Hide text for guide");
+  }
+}
---
+++ public/assets/webxr/webxr_basic.js
@@ -0,0 +1,31 @@
+import {WebXR} from '/assets/webxr/webxr.js';
+import {QueryArgs} from '/assets/webxr/util/query-args.js';
+import {SkyboxNode} from '/assets/webxr/render/nodes/skybox.js';
+
+// If requested, use the polyfill to provide support for mobile devices
+// and devices which only support WebVR.
+import WebXRPolyfill from '/assets/webxr/third-party/webxr-polyfill/build/webxr-polyfill.module.js';
+if (QueryArgs.getBool('usePolyfill', true)) {
+  let polyfill = new WebXRPolyfill();
+}
+
+// WebXR setup
+let app = new WebXR({referenceSpace: 'local-floor'});
+
+// Fullscreen icon
+let imgFull = app.createIcon("fullscreen-button.png", "fullscreen_toggle");
+imgFull.onclick = function() { app.toggleFullScreen() };
+
+// Options icon
+let imgOptions = app.createIcon("options-button.png", "options_toggle");
+imgOptions.onclick = function() { app.toggleOptions() };
+
+document.body.append(imgFull, imgOptions);
+document.getElementById('vr_button').appendChild(app.xrButton.domElement);
+
+app.scene.addNode(new SkyboxNode({
+  url: 'assets/media/sample/rico_test.jpg'
+}));
+
+// Start the XR application.
+app.run();
